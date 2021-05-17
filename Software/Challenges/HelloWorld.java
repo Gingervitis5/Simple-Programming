96b86f7d9b51bf866a2f4fc5ba186de5128c9c2a
Mix <gmix@eagles.ewu.edu>
2021-05-17 11:58:31 -0700

    Testing git connection

--- Software/Challenges/HelloWorld.java
+++ Software/Challenges/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("\nHello World!\n");
+        System.out.println("\nHello There World!\n");
     }
 }
